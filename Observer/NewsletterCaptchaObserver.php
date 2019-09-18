9fb06c243883f9fd27b2ef2fd23e1f5d2e1e1287
Kaarel <kaarel.rooparg@gmail.com>
2019-09-18 13:10:43 +0300

    Remove captcha validation when plugin is disabled

--- Observer/NewsletterCaptchaObserver.php
+++ Observer/NewsletterCaptchaObserver.php
@@ -2,18 +2,20 @@
 
 namespace Smaily\SmailyForMagento\Observer;
 
-use Magento\Framework\App\ActionFlag;
-use Magento\Framework\App\Response\RedirectInterface;
-use Magento\Framework\Message\ManagerInterface;
-use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\App\Request\DataPersistorInterface;
-use Magento\Framework\App\ObjectManager;
 use Magento\Captcha\Observer\CaptchaStringResolver;
 use Magento\Captcha\Helper\Data as Helper;
+use Magento\Framework\App\ActionFlag;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\App\Response\RedirectInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Event\ObserverInterface;
+use Smaily\SmailyForMagento\Helper\Data as SmailyHelper;
 
 class NewsletterCaptchaObserver implements ObserverInterface
 {
 
+    protected $smailyHelper;
     protected $helper;
     protected $actionFlag;
     protected $messageManager;
@@ -26,8 +28,10 @@ class NewsletterCaptchaObserver implements ObserverInterface
         ActionFlag $actionFlag,
         ManagerInterface $messageManager,
         RedirectInterface $redirect,
-        CaptchaStringResolver $captchaStringResolver
+        CaptchaStringResolver $captchaStringResolver,
+        SmailyHelper $smailyHelper
     ) {
+        $this->smailyHelper = $smailyHelper;
         $this->helper = $helper;
         $this->actionFlag = $actionFlag;
         $this->messageManager = $messageManager;
@@ -45,7 +49,7 @@ class NewsletterCaptchaObserver implements ObserverInterface
     {
         $formId = 'smaily_captcha';
         $captcha = $this->helper->getCaptcha($formId);
-        if ($captcha->isRequired()) {
+        if ($captcha->isRequired() && $this->smailyHelper->isEnabled()) {
             $controller = $observer->getControllerAction();
             if (!$captcha->isCorrect($this->captchaStringResolver->resolve($controller->getRequest(), $formId))) {
                 $this->messageManager->addError(__('Incorrect CAPTCHA.'));

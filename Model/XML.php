be75ac8daf251140cb8fcdef783099c510cb7170
Kait Todesk <kaittodesk@users.noreply.github.com>
2025-03-20 17:18:01 +0200

    Release 2.7.6 (#119)
    * rss: Fix RSS feed rendering fails with missing description value (#118)

--- Model/XML.php
+++ Model/XML.php
@@ -8,19 +8,19 @@ class XML extends \SimpleXMLElement
      * Add child node with CDATA wrapped value.
      *
      * @param string $name
-     * @param string|null $value
+     * @param string $value
      * @param string|null $namespace
      * @access public
      * @return \SimpleXMLElement|null
      */
-    public function addChildWithCDATA($name, $value = null, $namespace = null)
+    public function addChildWithCDATA($name, $value = "", $namespace = null)
     {
         $child = $this->addChild($name, null, $namespace);
 
         if ($child !== null) {
             $node = dom_import_simplexml($child);
             $document = $node->ownerDocument;
-            $node->appendChild($document->createCDATASection($value));
+            $node->appendChild($document->createCDATASection((string) $value));
         }
 
         return $child;
